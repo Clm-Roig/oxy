5b1613495842d654d3a7d2fe3b9489983053ed74
Clément Roig <clm.roig@gmail.com>
2018-04-05 17:11:26 +0200

    Tentative de fix... nop

--- oxyRN/index.ios.js
+++ oxyRN/index.ios.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './app/index';
 
-AppRegistry.registerComponent('RNDemo', () => App);
+AppRegistry.registerComponent('oxyRN', () => App);
