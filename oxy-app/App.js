db41a25fc7951dea6a6b27e9fda9c2be0f25e8bd
Clément Roig <clm.roig@gmail.com>
2018-04-07 10:46:08 +0200

    Connecting both, loading...

--- oxy-app/App.js
+++ oxy-app/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Meteor from 'react-native-meteor';
+const SERVER_URL = 'ws://localhost:3000/websocket';
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Bienvenue sur Oxy !</Text>
-      </View>
-    );
-  }
+
+    componentWillMount() {
+        Meteor.connect(SERVER_URL);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+            <Text>Bienvenue sur Oxy !</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
