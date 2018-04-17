d2aca791d66c02fdad58e565a106a5539130d225
Unknown <hugo.fazio@aol.com>
2018-04-17 18:21:26 +0200

    fix typo myButton

--- oxy-app/app/containers/addSandwich.js
+++ oxy-app/app/containers/addSandwich.js
@@ -8,7 +8,7 @@ import {
     TouchableOpacity
 } from 'react-native';
 
-import { MyButton } from '../components/MyButton';
+import { myButton } from '../components/myButton';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Meteor, { createContainer } from 'react-native-meteor';
