ca4b75c49fa87cfe7a19b9ca236c68e6774a3a8b
Clément Roig <clm.roig@gmail.com>
2018-04-18 09:47:23 +0200

    Settings Model

--- oxy-app/app/config/settings.js
+++
@@ -1,2 +0,0 @@
-// TODO : à changer avec sa propre IP
-export const SERVER_URL = 'ws://172.24.196.199:3000/websocket';
---
+++ oxy-app/app/config/settingsMODEL.js
@@ -0,0 +1,3 @@
+// TODO : à changer avec sa propre IP
+const SERVER_IP = '';
+export const SERVER_URL = 'ws://'+SERVER_IP+':3000/websocket';
